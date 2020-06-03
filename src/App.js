38796813ce4849020264b17b921718dbe607d8e6
Kato <i201622650@cibertec.edu.pe>
2020-06-02 21:09:35 -0500

    cubo transparente

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
+import * as THREE from 'three';
 import './App.css';
+
+let escena, camara, renderer, cubo;
+
+function init() {
+  //creando lo escena
+  escena = new THREE.Scene();
+  escena.background = new THREE.Color(0x2a3b4c);
+
+  //creando la camara
+  camara = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight);
+
+  //renderizando
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  //agregando geometria
+  let geometria = new THREE.BoxGeometry();
+  let material =  new THREE.MeshBasicMaterial({color: 0x00ff00, wireframe: true});
+  cubo =  new THREE.Mesh(geometria, material);
+
+  //agregando el cubo a la escena
+  escena.add(cubo);
+
+  camara.position.z = 5;
+
+  animate();
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  //rotacion del cubo
+  cubo.rotation.x += 0.01;
+  cubo.rotation.y += 0.01;
+
+  renderer.render(escena, camara);
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {init()}
     </div>
   );
 }
